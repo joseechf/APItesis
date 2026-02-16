69bb9b78df85f6df33e837ea9eb9a6408514a55f
joseechf <josechdev@gmail.com>
2026-02-16 08:09:40 -0500

    adaptacion inicializar para supabase

--- src/bdPostgresql/inicializar.js
+++ src/bdPostgresql/inicializar.js
@@ -2,31 +2,39 @@ import pg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
+
 let pool = null;
 
-export default async function inicializar(){
-    if(!pool){
+export default async function inicializar() {
+
+    if (!pool) {
         pool = new pg.Pool({
-            host:process.env.HOST,
-            port:process.env.POSTGRES_PORT,
-            database: process.env.POSTGRES_DB,
-            user: process.env.POSTGRES_USER,
-            password: process.env.POSTGRES_PASSWORD,
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false,
+            },
         });
-        try {
-            const client = await pool.connect();
-            return {status:200,data:client};
-        } catch (error) {
-            const errorFormateado = {
+    }
+
+    try {
+        const client = await pool.connect();
+
+        return {
+            status: 200,
+            data: client,
+        };
+
+    } catch (error) {
+
+        return {
+            status: 500,
+            error: {
                 code: error.code,
                 tablaAfectada: error.table,
                 constraint: error.constraint,
                 message: error.message,
             }
-            return errorFormateado;
-        }
-    }else{
-        const client = await pool.connect();
-        return {status:200,data:client};
+        };
+
     }
 }
